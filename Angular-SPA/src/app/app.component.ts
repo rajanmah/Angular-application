198be7dc643b23ea030f34434ad8fd39358ff6b1
rajanmah <146156379+rajanmah@users.noreply.github.com>
2024-01-06 10:20:41 -0500

    Angular Environment Set-up

---
+++ Angular-SPA/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    HomeComponent,
+    HeaderComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Angular-SPA';
+}
